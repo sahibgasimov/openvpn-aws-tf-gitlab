35a809bdbd4a5a32f5d74843a317c78bb63fcfea
SG <100177153+sahibgasimov@users.noreply.github.com>
2023-01-09 00:12:22 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,13 @@
-
-
 # AWS Provider
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.aws_region
+  # access_key = var.aws_access_key
+  # secret_key = var.aws_secret_key
+  region     = var.region 
 }
-# If you are using domain on different account you can use aws alias I called it Main AWS Provider
-provider "aws" {
-  alias      = "main"
-  access_key = var.aws_access_key_main
-  secret_key = var.aws_secret_key_main
-  region     = var.aws_region_main
-}
+# Main AWS Provider
+# provider "aws" {
+#   alias      = "main"
+#   access_key = var.aws_access_key
+#   secret_key = var.aws_secret_key
+#   region     = var.aws_region
+# }

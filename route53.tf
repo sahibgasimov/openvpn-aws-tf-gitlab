3443fb99002e4e0181a0caf93a1c16d1b79033fe
SG <100177153+sahibgasimov@users.noreply.github.com>
2023-01-09 00:10:29 -0600

    Update route53.tf

--- route53.tf
+++ route53.tf
@@ -1,12 +1,12 @@
 # Reference to the Route 53 Main Public Zone
 data "aws_route53_zone" "hostedzone" {
   name         = var.domain
-  provider     = aws.main
+  # provider     = aws.main
   private_zone = false
 }
 # Create Route 53 A Record for the Load Balancer in the Main Zone
 resource "aws_route53_record" "pan" {
-  provider = aws.main
+  # provider = aws.main
   zone_id  = data.aws_route53_zone.hostedzone.id
   name     = "${var.subdomain}.${data.aws_route53_zone.hostedzone.name}"
   type     = "A"
